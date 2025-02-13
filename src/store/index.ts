3c4129b6124a1555edc0983024067d187ae01322
Nastassia Ananka <anast.ananko@gmail.com>
2025-02-13 23:53:25 +0100

    feat: add redux-persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
 import { rickandmortyApi } from './services/rickandmortyApi';
-import uiStateReducer from './features/uiStateSlice';
+import uiStateSlice from './features/uiStateSlice';
+
+const uiStatePersistConfig = {
+  key: 'uiState',
+  storage: storage,
+};
+
+export const persistedReducer = persistReducer(
+  uiStatePersistConfig,
+  uiStateSlice
+);
 
 export const store = configureStore({
   reducer: {
     [rickandmortyApi.reducerPath]: rickandmortyApi.reducer,
-    uiState: uiStateReducer,
+    uiState: persistedReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }).concat(rickandmortyApi.middleware),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
